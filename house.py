eb6be8a812222737854ef08029f78a480fd87ee6
Arafat4341 <mdyasinarafatyen2@gmail.com>
2019-01-29 23:58:15 -0800

    removing zero variance variables

--- house.py
+++ house.py
@@ -1,9 +1,13 @@
 import pandas as pd
+from sklearn.feature_selection import VarianceThreshold
+
+def dataProcessing():
+	sel = VarianceThreshold(threshold=(.8 * (1 - .8)))
+	sel.fit_transform(Data1)
+	print(Data1.shape)
 
 Data1 = pd.read_csv('data/train.csv')
 Data2 = pd.read_csv('data/test.csv')
-
-fcols = ['']
-
-
 y = Data1.SalePrice
+
+dataProcessing()
