3428da88fdf20543bee3cb0a2f7ec3b3f8e518ca
Arafat4341 <mdyasinarafatyen2@gmail.com>
2019-02-03 12:19:48 -0800

    Data processing running

--- house.py
+++ house.py
@@ -1,13 +1,30 @@
 import pandas as pd
 from sklearn.feature_selection import VarianceThreshold
+from sklearn.tree import DecisionTreeRegressor
 
-def dataProcessing():
+"""
+feature reducted:
+Street, Alley, LotShape, LandContour, Utilities, LotConfig, LandSlope, Condition1, Condition2,
+BldgType, YearRemodAdd, RoofStyle, RoofMatl, Exterior2nd, 1terCond, BsmtCond, BsmtFinType2, Heating,
+Ce1tralAir, GarageQual, GarageCond, 2avedDrive, PoolArea, PoolQC, Fence, MiscFeature, 
+
+"""
+
+def dataProcessing(Data):
 	sel = VarianceThreshold(threshold=(.8 * (1 - .8)))
-	sel.fit_transform(Data1)
-	print(Data1.shape)
+	return sel.fit_transform(Data)
+
 
 Data1 = pd.read_csv('data/train.csv')
 Data2 = pd.read_csv('data/test.csv')
-y = Data1.SalePrice
 
-dataProcessing()
+Train = dataProcessing(Data1)
+
+fcols = ['MSSubClass', 'LotArea']
+
+"""X = Data1[fcols]
+y = Data1.SalePrice
+val_x = Data2[fcols]
+
+reg = DecisionTreeRegressor()
+reg.fit(X, y)"""
