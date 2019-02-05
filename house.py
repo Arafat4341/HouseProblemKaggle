c8668fe65eed3b564061ac77f13eb83bc20c82d5
Arafat4341 <mdyasinarafatyen2@gmail.com>
2019-02-05 00:04:18 -0800

    trained model

--- house.py
+++ house.py
@@ -1,14 +1,9 @@
 import pandas as pd
 from sklearn.feature_selection import VarianceThreshold
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
 
-"""
-feature reducted:
-Street, Alley, LotShape, LandContour, Utilities, LotConfig, LandSlope, Condition1, Condition2,
-BldgType, YearRemodAdd, RoofStyle, RoofMatl, Exterior2nd, 1terCond, BsmtCond, BsmtFinType2, Heating,
-Ce1tralAir, GarageQual, GarageCond, 2avedDrive, PoolArea, PoolQC, Fence, MiscFeature, 
-
-"""
 
 def dataProcessing(Data):
 	sel = VarianceThreshold(threshold=(.8 * (1 - .8)))
@@ -18,13 +13,25 @@ def dataProcessing(Data):
 Data1 = pd.read_csv('data/train.csv')
 Data2 = pd.read_csv('data/test.csv')
 
-Train = dataProcessing(Data1)
+fcols = ['LotArea', 'OverallQual', 'YearBuilt', '1terQual', 'HeatingQC',
+       'GrLivArea', 'KitchenQual', 'TotRmsAbvGrd', 'GarageArea']
 
-fcols = ['MSSubClass', 'LotArea']
+reg = RandomForestRegressor(max_depth=8, random_state=0, n_estimators=150)
 
-"""X = Data1[fcols]
+X = Data1[fcols]
 y = Data1.SalePrice
-val_x = Data2[fcols]
 
-reg = DecisionTreeRegressor()
-reg.fit(X, y)"""
+train_x, val_x, train_y, val_y = train_test_split(X, y, test_size = 0.4, random_state = 0)
+
+"""reg.fit(train_x, train_y)
+pred = reg.predict(val_x)"""
+
+reg.fit(X, y)
+pred = reg.predict(X)
+
+error = mean_absolute_error(y, pred)
+
+print(error)
+
+
+
