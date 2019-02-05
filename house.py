e6ac9145b5a63f577b5f96ab8cf92e7aaa13cda7
Arafat4341 <mdyasinarafatyen2@gmail.com>
2019-02-05 09:29:50 -0800

    made prediction

--- house.py
+++ house.py
@@ -20,18 +20,22 @@ reg = RandomForestRegressor(max_depth=8, random_state=0, n_estimators=150)
 
 X = Data1[fcols]
 y = Data1.SalePrice
+test_x = Data2[fcols]
+hid = Data2.Id
 
-train_x, val_x, train_y, val_y = train_test_split(X, y, test_size = 0.4, random_state = 0)
+"""train_x, val_x, train_y, val_y = train_test_split(X, y, test_size = 0.4, random_state = 0)
 
-"""reg.fit(train_x, train_y)
+reg.fit(train_x, train_y)
 pred = reg.predict(val_x)"""
 
 reg.fit(X, y)
-pred = reg.predict(X)
+pred = reg.predict(test_x)
 
-error = mean_absolute_error(y, pred)
+"""error = mean_absolute_error(y, pred)
+print(error)"""
 
-print(error)
+df = pd.DataFrame({'Id':hid, 'SalePrice':pred})
+df.to_csv('result.csv', sep=',', encoding='utf-8')
 
 
 
